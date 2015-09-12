55c5f82c682c64fae67e855e773c60349915fc68
Mauricio Soares <m.soares2603@gmail.com>
2015-09-12 14:04:58 -0300

    first commit

---
+++ app/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import test from './test';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div> teste </div>
+    );
+  }
+};
+
+React.render(<App />, document.body);
